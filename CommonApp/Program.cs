530870d5212e0f1c398866cac64edbc2e9468cf1
mamunur1296 <mamunurrushid60@gmail.com>
2023-09-25 12:31:36 +0100

    first Class1

---
+++ CommonApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using StudentLibrary;
+using TeachersLibrary;
+class Program
+{
+    static void Main()
+    {
+        // Use the Student class from StudentLibrary
+        Student student = new Student
+        {
+            Name = "John Doe",
+            Age = 20
+        };
+
+        // Use the Teacher class from TeachersLibrary
+        Teacher teacher = new Teacher
+        {
+            Name = "Mrs. Smith",
+            Subject = "Math"
+        };
+
+        Console.WriteLine($"Student: {student.Name}, Age: {student.Age}");
+        Console.WriteLine($"Teacher: {teacher.Name}, Subject: {teacher.Subject}");
+    }
+}
---
+++ TeachersLibrary/Teacher.cs
@@ -0,0 +1,8 @@
+﻿namespace TeachersLibrary
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+    }
+}
